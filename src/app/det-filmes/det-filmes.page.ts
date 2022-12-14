786ee72e3c42da2b431187af9bb5be3c1291ea51
Valeria <valleriaduraes@hotmail.com>
2022-12-13 21:22:50 -0300

    13122022-2

--- src/app/det-filmes/det-filmes.page.ts
+++ src/app/det-filmes/det-filmes.page.ts
@@ -7,41 +7,37 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./det-filmes.page.scss'],
 })
 export class DetFilmesPage implements OnInit {
-
-  information: string = "";
+  information: string = '';
   id: number = 0;
-  title: string = "";
-  generate: string = "";
+  title: string = '';
+  generate: string = '';
   duration: number = 0;
-  img: string = "";
+  img: string = '';
 
-  constructor( private route: ActivatedRoute) { }
-
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      console.log(params['id'])
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
       this.id = params['id'];
-      console.log("Passou no detalhe");
-  })
+    });
 
-        this.carregarDetalhes();    
-      }
+    this.carregarDetalhes();
+  }
 
+  async carregarDetalhes() {
+    let url = 'http://lmcapifilmes.herokuapp.com/api/production/' + this.id;
 
-      async carregarDetalhes() {
-        let url = 'http://lmcapifilmes.herokuapp.com/api/production/' + this.id;
-    
-        fetch(url)
-          .then((resposta) => {
-            return resposta.json();
-          })
-          .then((json) => {
-            this.img = json.Production.photo_link;
-            this.title = json.Production.title;
-            this.duration = json.Production.duration;
-            this.information = json.Production.description;
-            this.generate = json.Production.genre;
-          });}
-
+    fetch(url)
+      .then((resposta) => {
+        return resposta.json();
+      })
+      .then((json) => {
+        this.img = json.Production.photo_link;
+        this.title = json.Production.title;
+        this.duration = json.Production.duration;
+        this.information = json.Production.description;
+        this.generate = json.Production.genre;
+      });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { FilmesService } from '../service/filmes.service';
 
 @Component({
@@ -7,34 +8,33 @@ import { FilmesService } from '../service/filmes.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
-
-  filmes:any = [];
-  destaque:any = [];
+  filmes: any = [];
+  destaque: any = [];
 
   constructor(
     private rota: Router,
-    private filmesService: FilmesService) {}
-  
+    private filmesService: FilmesService,
+    private loadingCtrl: LoadingController
+  ) {}
 
-    ngOnInit(){
-      console.log('passou no home');
-      this.carregarFilmes();
-    }
-    async carregarFilmes(){
-      this.filmes  = await this.filmesService.getProductions();
-      console.log("filmes carregados", this.filmes)
-      const [firstKey] = Object.keys(this.filmes);
-      this.destaque = this.filmes[firstKey];
-      this.filmes.splice(firstKey, 1)
-      console.log('firstKey',firstKey)
-      console.log('destaque',this.destaque)
-      console.log('filmes',this.filmes)
-    }
-    
-  detalhesFilme(paramID){
-  console.log("passou"+paramID);
-  this.rota.navigate(['/detales-fil-principal', {id:paramID}])
- }
+  ngOnInit() {
+    console.log('passou no home');
+    this.carregarFilmes();
+  }
+  async carregarFilmes() {
+    this.filmes = await this.filmesService.getProductions();
+    console.log('filmes carregados', this.filmes);
+    const [firstKey] = Object.keys(this.filmes);
+    this.destaque = this.filmes[firstKey];
+    this.filmes.splice(firstKey, 1);
+    console.log('firstKey', firstKey);
+    console.log('destaque', this.destaque);
+    console.log('filmes', this.filmes);
+  }
+
+  detalhesFilme(paramID) {
+    console.log('passou' + paramID);
+    this.rota.navigate(['/det-filmes', { id: paramID }]);
+  }
 }
